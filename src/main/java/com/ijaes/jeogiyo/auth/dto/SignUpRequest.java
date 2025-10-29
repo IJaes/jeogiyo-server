33492f167197c3d717e7987e257f994436bcca4d
hoonssac <Hoonssac.dev@gmail.com>
2025-10-29 18:00:24 +0900

    fix: #3 사장님 여부 필드 매핑 오류 해결

--- src/main/java/com/ijaes/jeogiyo/auth/dto/SignUpRequest.java
+++ src/main/java/com/ijaes/jeogiyo/auth/dto/SignUpRequest.java
@@ -1,5 +1,6 @@
 package com.ijaes.jeogiyo.auth.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +14,6 @@ public class SignUpRequest {
     private String name;
     private String address;
     private String phoneNumber;
+    @JsonProperty("isOwner")
     private boolean isOwner;
 }
