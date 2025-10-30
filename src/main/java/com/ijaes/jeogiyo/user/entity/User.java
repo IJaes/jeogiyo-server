00208e870548cc787de5165590c3305ad10e5d06
hoonssac <Hoonssac.dev@gmail.com>
2025-10-30 16:47:53 +0900

    feat: #14 사용자와 연관 관계 설정

--- src/main/java/com/ijaes/jeogiyo/user/entity/User.java
+++ src/main/java/com/ijaes/jeogiyo/user/entity/User.java
@@ -1,6 +1,8 @@
 package com.ijaes.jeogiyo.user.entity;
 
 import com.ijaes.jeogiyo.common.entity.BaseEntity;
+import com.ijaes.jeogiyo.store.entity.Store;
+
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
@@ -41,6 +43,10 @@ public class User extends BaseEntity implements UserDetails {
 
     @Column(nullable = false)
     private boolean isOwner;
+
+    @OneToOne
+    @JoinColumn(name = "store_id", nullable = true, unique = true)
+    private Store store;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
