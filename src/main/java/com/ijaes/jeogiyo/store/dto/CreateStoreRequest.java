5a0bfb920c3593608921ab3888fd4690f5f63779
hoonssac <Hoonssac.dev@gmail.com>
2025-10-30 16:46:23 +0900

    feat: #14 매장 생성 요청/응답 데이터 구조 설계

---
+++ src/main/java/com/ijaes/jeogiyo/store/dto/CreateStoreRequest.java
@@ -0,0 +1,30 @@
+package com.ijaes.jeogiyo.store.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Schema(description = "가게 생성 요청")
+public class CreateStoreRequest {
+
+	@Schema(description = "사업자번호", example = "123-45-67890")
+	private String businessNumber;
+
+	@Schema(description = "가게명", example = "소문난 국밥집")
+	private String name;
+
+	@Schema(description = "주소", example = "서울시 강남구 역삼동")
+	private String address;
+
+	@Schema(description = "설명", example = "뜨근뜨끈한 국물 한 사발 먹고 가세요")
+	private String description;
+
+	@Schema(description = "카테고리", example = "KOREAN")
+	private String category;
+}
