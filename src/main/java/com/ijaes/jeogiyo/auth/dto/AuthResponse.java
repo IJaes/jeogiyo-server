9ae672c00e16a687da7931148091e486d314d73e
hoonssac <Hoonssac.dev@gmail.com>
2025-10-30 11:05:58 +0900

    feat: #7 스키마 레벨 Swagger 설정 추가

--- src/main/java/com/ijaes/jeogiyo/auth/dto/AuthResponse.java
+++ src/main/java/com/ijaes/jeogiyo/auth/dto/AuthResponse.java
@@ -1,5 +1,6 @@
 package com.ijaes.jeogiyo.auth.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +10,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Schema(description = "인증 응답")
 public class AuthResponse {
+    @Schema(description = "응답 메시지", example = "회원가입이 성공했습니다.!")
     private String message;
+
+    @Schema(description = "JWT 토큰", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
     private String token;
+
+    @Schema(description = "성공 여부", example = "true")
     private boolean success;
+
+    @Schema(description = "사용자 역할", example = "ROLE_USER")
     private String role;
 }
