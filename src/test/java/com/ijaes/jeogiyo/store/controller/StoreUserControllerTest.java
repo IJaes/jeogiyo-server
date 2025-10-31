3d4ab1ee991130c44cdb14cc6bf21b2b959a37d7
hoonssac <Hoonssac.dev@gmail.com>
2025-10-31 11:39:54 +0900

    fix: #14 테스트 플래그가 Gradle에서 작동하지 않는 문제 해결

---
+++ src/test/java/com/ijaes/jeogiyo/store/controller/StoreUserControllerTest.java
@@ -0,0 +1,12 @@
+package com.ijaes.jeogiyo.store.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class StoreUserControllerTest {
+
+	@Test
+	void getAllStores() {
+	}
+}
