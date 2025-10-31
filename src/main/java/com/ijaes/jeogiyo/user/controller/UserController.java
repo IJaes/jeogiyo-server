bea861bcf04f7e8306c52f9d764eab969559d0ed
hoonssac <Hoonssac.dev@gmail.com>
2025-10-31 15:37:40 +0900

    refactor: #14 사용자 정보 수정 DTO 클래스명 변경

--- src/main/java/com/ijaes/jeogiyo/user/controller/UserController.java
+++ src/main/java/com/ijaes/jeogiyo/user/controller/UserController.java
@@ -3,7 +3,7 @@ package com.ijaes.jeogiyo.user.controller;
 import com.ijaes.jeogiyo.user.dto.request.UpdateAddressRequest;
 import com.ijaes.jeogiyo.user.dto.request.UpdatePasswordRequest;
 import com.ijaes.jeogiyo.user.dto.request.UpdatePhoneNumberRequest;
-import com.ijaes.jeogiyo.user.dto.response.SimpleUpdateResponse;
+import com.ijaes.jeogiyo.user.dto.response.UserUpdateResponse;
 import com.ijaes.jeogiyo.user.dto.response.UserInfoResponse;
 import com.ijaes.jeogiyo.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -31,22 +31,22 @@ public class UserController {
 
     @PatchMapping("/address")
     @Operation(summary = "주소 수정", description = "사용자의 주소를 수정합니다", security = @SecurityRequirement(name = "bearer-jwt"))
-    public ResponseEntity<SimpleUpdateResponse> updateAddress(Authentication authentication, @RequestBody UpdateAddressRequest request) {
-        SimpleUpdateResponse response = userService.updateAddress(authentication, request);
+    public ResponseEntity<UserUpdateResponse> updateAddress(Authentication authentication, @RequestBody UpdateAddressRequest request) {
+        UserUpdateResponse response = userService.updateAddress(authentication, request);
         return ResponseEntity.ok(response);
     }
 
     @PatchMapping("/phone")
     @Operation(summary = "전화번호 수정", description = "사용자의 전화번호를 수정합니다", security = @SecurityRequirement(name = "bearer-jwt"))
-    public ResponseEntity<SimpleUpdateResponse> updatePhoneNumber(Authentication authentication, @RequestBody UpdatePhoneNumberRequest request) {
-        SimpleUpdateResponse response = userService.updatePhoneNumber(authentication, request);
+    public ResponseEntity<UserUpdateResponse> updatePhoneNumber(Authentication authentication, @RequestBody UpdatePhoneNumberRequest request) {
+        UserUpdateResponse response = userService.updatePhoneNumber(authentication, request);
         return ResponseEntity.ok(response);
     }
 
     @PatchMapping("/password")
     @Operation(summary = "비밀번호 수정", description = "사용자의 비밀번호를 수정합니다", security = @SecurityRequirement(name = "bearer-jwt"))
-    public ResponseEntity<SimpleUpdateResponse> updatePassword(Authentication authentication, @RequestBody UpdatePasswordRequest request) {
-        SimpleUpdateResponse response = userService.updatePassword(authentication, request);
+    public ResponseEntity<UserUpdateResponse> updatePassword(Authentication authentication, @RequestBody UpdatePasswordRequest request) {
+        UserUpdateResponse response = userService.updatePassword(authentication, request);
         return ResponseEntity.ok(response);
     }
 }
