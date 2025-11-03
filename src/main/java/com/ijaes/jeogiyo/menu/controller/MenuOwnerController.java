cdc77f10ed92a2da99253f40f5ca9687c107faca
hoonssac <Hoonssac.dev@gmail.com>
2025-11-03 13:20:10 +0900

    feat: #20 스웨거 태그 추가

--- src/main/java/com/ijaes/jeogiyo/menu/controller/MenuOwnerController.java
+++ src/main/java/com/ijaes/jeogiyo/menu/controller/MenuOwnerController.java
@@ -11,16 +11,21 @@ import com.ijaes.jeogiyo.menu.dto.request.CreateMenuRequest;
 import com.ijaes.jeogiyo.menu.dto.response.MenuResponse;
 import com.ijaes.jeogiyo.menu.service.MenuOwnerService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/v1/owner")
 @RequiredArgsConstructor
+@Tag(name = "사장님")
 public class MenuOwnerController {
 
 	private final MenuOwnerService menuOwnerService;
 
 	@PostMapping("/menus")
+	@Operation(summary = "메뉴 등록", description = "메뉴를 등록합니다", security = @SecurityRequirement(name = "bearer-jwt"))
 	public MenuResponse createMenu(@RequestBody CreateMenuRequest request, Authentication authentication) {
 		return menuOwnerService.createMenu(request, authentication);
 	}
