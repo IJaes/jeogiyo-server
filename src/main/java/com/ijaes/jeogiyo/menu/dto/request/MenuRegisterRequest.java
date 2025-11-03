1265903c60bc6f6d16911cf503cfad3507053dab
hoonssac <Hoonssac.dev@gmail.com>
2025-11-03 11:32:59 +0900

    feat: #20 메뉴 엔티티 및 요청 DTO

---
+++ src/main/java/com/ijaes/jeogiyo/menu/dto/request/MenuRegisterRequest.java
@@ -0,0 +1,29 @@
+package com.ijaes.jeogiyo.menu.dto.request;
+
+import java.util.UUID;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@Schema(description = "메뉴 등록")
+public class MenuRegisterRequest {
+
+	@NotBlank(message = "메뉴명은 필수입니다.")
+	@Schema(description = "메뉴명", example = "순대국밥")
+	private String name;
+
+	@NotBlank(message = "메뉴 설명은 필수입니다.")
+	@Schema(description = "메뉴 설명", example = "속이 꽌 찬 순대와 1200시간 이상 끓인 육수로 최고의 건강과 맛을 선사합니다.")
+	private String description;
+
+	@NotNull(message = "가격은 필수입니다.")
+	@Positive(message = "가격은 0보다 커야 합니다.")
+	@Schema(description = "메뉴 가격", example = "12000")
+	private Integer price;
+}
--- src/main/java/com/ijaes/jeogiyo/menu/entity/Menu.java
+++ src/main/java/com/ijaes/jeogiyo/menu/entity/Menu.java
@@ -1,8 +1,18 @@
 package com.ijaes.jeogiyo.menu.entity;
 
-import com.ijaes.jeogiyo.common.entity.BaseEntity;
+import java.util.UUID;
 
+import com.ijaes.jeogiyo.common.entity.BaseEntity;
+import com.ijaes.jeogiyo.store.entity.Store;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,5 +27,20 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Menu extends BaseEntity {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.UUID)
+	private UUID id;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "store_id", nullable = false)
+	private Store store;
+
+	@Column(nullable = false)
+	private String name;
+
+	@Column(nullable = false)
+	private String description;
+
+	@Column(nullable = false)
+	private Integer price;
 }
