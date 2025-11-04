81355faf6716cf9184c1a3107225677ad2eda5f0
hoonssac <Hoonssac.dev@gmail.com>
2025-11-04 11:53:55 +0900

    feat: #28 메뉴 not found 에러 코드 추가

--- src/main/java/com/ijaes/jeogiyo/common/exception/ErrorCode.java
+++ src/main/java/com/ijaes/jeogiyo/common/exception/ErrorCode.java
@@ -54,10 +54,11 @@ public enum ErrorCode {
 	STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "S-002", "매장을 찾을 수 없습니다. 먼저 매장을 등록해주세요."),
 	INVALID_CATEGORY(HttpStatus.BAD_REQUEST, "S-003", "유효하지 않은 카테고리입니다. (KOREAN, JAPANESE, CHINESE, ITALIAN)"),
 
-	//리뷰 관련 (W-xxx)
+	// 리뷰 관련 (W-xxx)
 	REVIEW_ALREADY_EXISTS(HttpStatus.CONFLICT, "W-001", "이미 해당 주문에 대한 리뷰가 작성되었습니다."),
 
-	;
+	// 메뉴 관련 (M-xxx)
+	MENU_NOT_FOUND(HttpStatus.NOT_FOUND, "M-001", "메뉴를 찾을 수 없습니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
