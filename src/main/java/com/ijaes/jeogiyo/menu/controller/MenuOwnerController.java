3ff40c31d5c79f0af0a2cd887d95f03fb3abba64
hoonssac <Hoonssac.dev@gmail.com>
2025-11-04 11:57:30 +0900

    feat: #28 메뉴 수정 비즈니스 로직 작성

--- src/main/java/com/ijaes/jeogiyo/menu/controller/MenuOwnerController.java
+++ src/main/java/com/ijaes/jeogiyo/menu/controller/MenuOwnerController.java
@@ -1,16 +1,21 @@
 package com.ijaes.jeogiyo.menu.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.parameters.P;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ijaes.jeogiyo.menu.dto.request.CreateMenuRequest;
+import com.ijaes.jeogiyo.menu.dto.request.UpdateMenuRequest;
 import com.ijaes.jeogiyo.menu.dto.response.MenuResponse;
 import com.ijaes.jeogiyo.menu.service.MenuOwnerService;
 
@@ -30,7 +35,8 @@ public class MenuOwnerController {
 
 	@PostMapping
 	@Operation(summary = "메뉴 등록", description = "메뉴를 등록합니다", security = @SecurityRequirement(name = "bearer-jwt"))
-	public ResponseEntity<MenuResponse> createMenu(@Valid @RequestBody CreateMenuRequest request, Authentication authentication) {
+	public ResponseEntity<MenuResponse> createMenu(@Valid @RequestBody CreateMenuRequest request,
+		Authentication authentication) {
 		MenuResponse menuResponse = menuOwnerService.createMenu(request, authentication);
 		return ResponseEntity.ok(menuResponse);
 	}
@@ -40,5 +46,12 @@ public class MenuOwnerController {
 	public ResponseEntity<List<MenuResponse>> getMenus(Authentication authentication) {
 		List<MenuResponse> menus = menuOwnerService.getMyMenus(authentication);
 		return ResponseEntity.ok(menus);
+	}
+
+	@PatchMapping("/{menuId}")
+	@Operation(summary = "메뉴 수정", description = "메뉴 정보를 수정합니다", security = @SecurityRequirement(name = "bearer-jwt"))
+	public ResponseEntity<MenuResponse> updateMenu(@PathVariable UUID menuId, @Valid @RequestBody UpdateMenuRequest request, Authentication authentication) {
+		MenuResponse menuResponse = menuOwnerService.updateMenu(menuId, request, authentication);
+		return ResponseEntity.ok(menuResponse);
 	}
 }
