a8122c5b538ee358a66a6ae8ef30f2d9cb1795d2
hoonssac <Hoonssac.dev@gmail.com>
2025-11-04 11:49:58 +0900

    feat: #28 수정할 메뉴 조회 쿼리 작성

--- src/main/java/com/ijaes/jeogiyo/menu/repository/MenuRepositoryCustom.java
+++ src/main/java/com/ijaes/jeogiyo/menu/repository/MenuRepositoryCustom.java
@@ -1,10 +1,13 @@
 package com.ijaes.jeogiyo.menu.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 import com.ijaes.jeogiyo.menu.entity.Menu;
 
 public interface MenuRepositoryCustom {
 	List<Menu> findByOwnerId(UUID ownerId);
+
+	Optional<Menu> findByIdAndOwnerId(UUID menuId, UUID ownerId);
 }
