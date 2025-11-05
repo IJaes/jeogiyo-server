f834c61e6dd678b5862cd8c17263fe7832987760
sojsnake <sojsnake@naver.com>
2025-11-05 11:46:22 +0900

    feat: #37 평점 재계산 이벤트 발생

---
+++ src/main/java/com/ijaes/jeogiyo/review/event/ReviewEvent.java
@@ -0,0 +1,21 @@
+package com.ijaes.jeogiyo.review.event;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ReviewEvent {
+
+	//매장 식별자
+	UUID storeId;
+
+	//이벤트 발생 원인(생성, 수정, 삭제)
+	EventType type;
+
+	//변경된 평점 값
+	Integer newRating;
+
+	//이전 평점 값
+	Integer oldRating;
+}
