3814fb8e11b6a6e96df773b2cff90447dc2c63c1
hoonssac <Hoonssac.dev@gmail.com>
2025-11-08 00:38:22 +0900

    fix: #49 메뉴 등록 DTO 기본 생성자 추가

--- src/main/java/com/ijaes/jeogiyo/menu/dto/request/CreateMenuRequest.java
+++ src/main/java/com/ijaes/jeogiyo/menu/dto/request/CreateMenuRequest.java
@@ -5,11 +5,15 @@ import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.PositiveOrZero;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Schema(description = "메뉴 등록")
 public class CreateMenuRequest {
 
